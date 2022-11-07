24341f47ddc2426ec26aec1af6445b0fe9187141
harshdavda99 <harshdavda(9@gmail.com>
2022-11-07 23:25:31 +0530

    added ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './components/authorisation/login/login.component';
+import { RegisterComponent } from './components/authorisation/register/register.component';
+import { ChatWindowComponent } from './components/Messages/chat-window/chat-window.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component'
+// import { ChatListComponent } from './components/Messages/chat-list/chat-list.component';
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'chatbox', component: ChatWindowComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/authorisation/login/login.component';
 import { RegisterComponent } from './components/authorisation/register/register.component';
+import { ChatWindowComponent } from './components/Messages/chat-window/chat-window.component';
+import { ChatListComponent } from './components/Messages/chat-list/chat-list.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ChatWindowComponent,
+    ChatListComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
